990d973a68c39618311463df07b7c7d9dd7b932d
Siddhesh Mhatre <sid1793@gmail.com>
2018-03-22 01:54:19 -0700

    complete implementation of capsnet reconstruction layer and loss layer remain

--- capsnet.py
+++ capsnet.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from torch.autograd import Variable
 
@@ -30,7 +31,25 @@ class CapsuleLayer(nn.Module):
     def forward(self, x):
 
         if self.num_route_weights != -1:
-            pass
+
+            # Multiply output of primarycaps with weight matrices for digitcaps
+            u = x[:, None, :, None, :] @ self.routing_weights[None, :, :, :, :]
+
+            logits = Variable(torch.zeros(*u.size()))
+
+            # Dynamic routing
+            for i in range(self.num_iterations):
+                probs = self.softmax(logits)
+                outputs = (probs * u).sum(dim=2, keepdim=True)
+                outputs = self.squash(outputs)
+
+                if i < self.num_iterations:
+                    change = (outputs * probs).sum(dim=-1, keepdim=True)
+
+                    # update logits based on dot product between u and v
+                    logits = logits + change
+
+            return outputs
         else:
             # If Primary capsule
             outputs = [conv_cap(x).view(x.size(0), self.output_channels, -1, 1)
@@ -39,3 +58,13 @@ class CapsuleLayer(nn.Module):
             outputs = outputs.transpose(-1, 1).contiguous()
             outputs = outputs.view(outputs.size(0), -1, self.output_channels)
             return outputs
+
+    def softmax(self, x, dim=1):
+        x_t = x.transpose(dim, len(x.size()) - 1)
+        softmax_output = F.softmax(x_t.contiguous().view(-1, x_t.size(-1)), dim=-1)
+        return softmax_output.view(*x_t.size()).transpose(dim, len(x_t.size()) - 1)
+
+    def squash(self, x, dim=2):
+        sqrd_norm = (x ** 2).sum(dim=dim, keepdim=True)
+        scale = sqrd_norm / (1 + sqrd_norm)
+        return scale * (x / torch.sqrt(sqrd_norm))
